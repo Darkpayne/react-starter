a539a65a537a421eecba8d96461b64295deaebce
Darkpayne <joshuaclifford4336@yahoo.com>
2022-10-24 09:56:25 +0100

    did some nav work

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div className='flex justify-between items-center bg-green-200 py-3 text-2xl'>
+    <div className='flex justify-between items-center bg-green-50 py-3 text-2xl'>
         <h1>Nav Starter</h1>
         <ul className='flex gap-x-2 mx-4'>
             <Link to="/"><li>Home</li></Link>
