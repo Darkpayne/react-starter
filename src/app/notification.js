11ef8091bcf5e0ae52d0e5beeea20c85176792df
joshua clifford <joshuaclifford4336@yahoo.com>
2022-11-24 18:17:04 +0100

    made changes to formik validation

--- src/app/notification.js
+++ src/app/notification.js
@@ -1,44 +1,46 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const useToastify = () => {
-    const createToast=(toastConfigObj)=>{
-        const {msg,dataType} = toastConfigObj
-            dataType || toast.error(msg , {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: true,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              });
+  const createToast = (toastConfigObj) => {
+    const { msg, dataType } = toastConfigObj;
+    dataType ||
+      toast.error(msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
 
-            dataType && toast.success(msg , {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: true,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              });
-    }
-    const notification = () =>{
-      return (
-        <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        />
-      )
-    }
-  return {createToast, notification}
-}
+    dataType &&
+      toast.success(msg, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+  };
+  return { createToast };
+};
 
+export const Notification = () => {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  );
+};
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,115 +1,174 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useFormik } from 'formik';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import { basicSchema } from "../validation/YupValidation";
+import { useToastify, Notification } from "../app/notification";
 
 const Signup = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <div className="mx-auto h-screen container flex justify-center items-center">
-          <div className="bg-white p-10 flex max-w-lg flex-col w-full shadow-xl rounded-xl">
-            <h2 className="text-2xl font-bold text-gray-800 text-left mb-5">
-              Sign Up
-            </h2>
-            <form  onSubmit={formik.handleSubmit}>
-              <div id="input" className="flex flex-col w-full my-5">
-                <label for="firstName" className="text-gray-500 mb-2"
-                  >First Name</label
-                >
-                <input
-                  type="text"
-                  id="firstName"
-                  placeholder="Please insert your username"
-                  className="appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  onChange={formik.handleChange}
-                  value={formik.values.firstName}
-                />
-              </div>
-              <div id="input" className="flex flex-col w-full my-5">
-                <label for="lastName" className="text-gray-500 mb-2"
-                  >Last Name</label
-                >
-                <input
-                  type="text"
-                  id="lastName"
-                  placeholder="Please insert your username"
-                  className="appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  onChange={formik.handleChange}
-                  value={formik.values.lastName}
-                />
-              </div>
-              <div id="input" className="flex flex-col w-full my-5">
-                <label for="email" className="text-gray-500 mb-2"
-                  >Email</label
-                >
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Please insert your username"
-                  className="appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                />
-              </div>
-              <div id="input" className="flex flex-col w-full my-5">
-                <label for="password" className="text-gray-500 mb-2"
-                  >Password</label
-                >
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Please insert your password"
-                  className="appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
-              </div>
-              <div id="button" className="flex flex-col w-full my-5">
-                <button
-                  type="submit"
-                  className="w-full py-4 bg-green-600 rounded-lg text-green-100"
-                >
-                  <div className="flex flex-row items-center justify-center">
-                    <div className="mr-2">
-                      <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                        ></path>
-                      </svg>
-                    </div>
-                    <div className="font-bold">Sign up</div>
-                  </div>
-                </button>
-                <div className="flex justify-evenly mt-5">
-                  
-                  <Link
-                    to="/signup"
-                    className="w-full text-center font-medium text-gray-500"
-                    >Sing In!</Link>
-                </div>
-              </div>
-            </form>
-          </div>
-    </div>
-  )
-}
+  const { createToast } = useToastify();
+  const onSubmit = (values, actions) => {
+    createToast({ msg: "success", dataType: true });
+    console.log(actions);
+    actions.resetForm();
+    alert(JSON.stringify(values, null, 2));
+  };
+  const { handleBlur, handleChange, handleSubmit, values, touched, errors } =
+    useFormik({
+      initialValues: {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: basicSchema,
+      onSubmit,
+    });
 
-export default Signup
+  console.log(errors);
+  return (
+    <div className="mx-auto container flex justify-center items-center">
+      <div className="bg-white px-10 py-5 my-10 flex max-w-md flex-col w-full shadow-xl rounded-xl">
+        <h2 className="text-2xl font-bold text-gray-800 text-left mb-5">
+          Sign Up
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div id="input" className="flex flex-col w-full my-5">
+            <label
+              htmlFor="username"
+              className="text-gray-500 flex justify-between items-center mb-2"
+            >
+              <span>Username</span>
+              {errors.username && touched.username && (
+                <p className="text-red-500 text-xs mx-2 mt-1 font-semibold">
+                  {errors.username}
+                </p>
+              )}
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Please insert your username"
+              className={`appearance-none border-2 bg-transparent rounded-lg px-4 py-2 placeholder-gray-400 outline-none  ${
+                errors.username && touched.username ? "border-red-500" : ""
+              } focus:shadow-lg`}
+              onChange={handleChange}
+              value={values.username}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div id="input" className="flex flex-col w-full my-5">
+            <label
+              htmlFor="email"
+              className="text-gray-500 flex justify-between items-center mb-2"
+            >
+              <span>Email</span>
+              {errors.email && touched.email && (
+                <p className="text-red-500 text-xs mx-2 mt-1 font-semibold">
+                  {errors.email}
+                </p>
+              )}
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Please insert your username"
+              className={` border-2 bg-transparent rounded-lg px-4 py-2 placeholder-gray-400  outline-none ${
+                errors.email && touched.email ? "border-red-500" : ""
+              } focus:shadow-lg`}
+              onChange={handleChange}
+              value={values.email}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div id="input" className="flex flex-col w-full my-5">
+            <label
+              htmlFor="password"
+              className="text-gray-500 flex justify-between items-center mb-2"
+            >
+              <span> Password</span>
+              {errors.password && touched.password && (
+                <p className="text-red-500 text-xs mx-2 mt-1 font-semibold">
+                  {errors.password}
+                </p>
+              )}
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Please insert your password"
+              className={`appearance-none border-2 bg-transparent rounded-lg px-4 py-2 placeholder-gray-400 outline-none  ${
+                errors.password && touched.password ? "border-red-500" : ""
+              } focus:shadow-lg`}
+              onChange={handleChange}
+              value={values.password}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div id="input" className="flex flex-col w-full my-5">
+            <label
+              htmlFor="confirmPassword"
+              className="text-gray-500 flex justify-between items-center mb-2"
+            >
+              <span> Confirm Password</span>
+              {errors.confirmPassword && touched.confirmPassword && (
+                <p className="text-red-500 text-xs mx-2 mt-1 font-semibold">
+                  {errors.confirmPassword}
+                </p>
+              )}
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              placeholder="Please confirm your password"
+              className={`appearance-none border-2 bg-transparent rounded-lg px-4 py-2 placeholder-gray-400 outline-none  ${
+                errors.confirmPassword && touched.confirmPassword
+                  ? "border-red-500"
+                  : ""
+              } focus:shadow-lg`}
+              onChange={handleChange}
+              value={values.confirmPassword}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div id="button" className="flex flex-col w-full my-5">
+            <button
+              type="submit"
+              className="w-full py-4 bg-green-600 rounded-lg text-green-100"
+            >
+              <div className="flex flex-row items-center justify-center">
+                <div className="mr-2">
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="font-bold">Sign up</div>
+              </div>
+            </button>
+            <div className="flex justify-evenly mt-5">
+              <Link
+                to="/signin"
+                className="w-full text-center font-medium text-gray-500"
+              >
+                Sign In!
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Notification />
+    </div>
+  );
+};
+
+export default Signup;
